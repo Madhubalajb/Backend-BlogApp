31e67c17a8d8d1c4b7e99bdfe7a85b3e75d33f8e
Madhubalajb <jbmadhumng@gmail.com>
2019-10-26 12:07:52 +0530

    refactor app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const express = require('express')
 const mongoose = require('mongoose')
@@ -20,5 +21,8 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app

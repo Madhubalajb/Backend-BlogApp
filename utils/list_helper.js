26a3a4789fb69a224d9bba80507c1d2ef0a93aa7
Madhubalajb <jbmadhumng@gmail.com>
2019-10-26 19:17:07 +0530

    unit test - ex 4.5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, current) => sum + current
+
+    return blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

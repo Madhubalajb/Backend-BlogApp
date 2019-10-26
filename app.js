b297cc1ed530b4e5e17eee923d2f63bf3088ae24
Madhubalajb <jbmadhumng@gmail.com>
2019-10-26 11:53:19 +0530

    refactor app

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+
+console.log(`connecting to ${config.MONGODB_URI}`)
+
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser:true})
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch(error => {
+        console.log(`error connection to MongoDB: ${error.message}`)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app

6754670961701ab4fea41d0454fc7b984ea974b4
Madhubalajb <jbmadhumng@gmail.com>
2019-10-27 10:50:07 +0530

    unit test

--- tests/favoriteBlog.test.js
+++ tests/favoriteBlog.test.js
@@ -1,25 +1,7 @@
 const listHelper = require('../utils/list_helper')
+const blogLists = require('../utils/blogLists')
 
 describe('favorite Blog', () => {
-    const lists = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-          },
-          {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            __v: 0
-          }
-        ]
-    
     test('favorite Blog test', () => {
         expect(listHelper.favoriteBlog(lists)).toEqual()
     })
--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -1,27 +1,19 @@
 const listHelper = require('../utils/list_helper')
+const blogLists = require('../utils/blogLists')
 
 describe('total likes', () => {
-    const lists = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-          },
-          {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            __v: 0
-          }
-        ]
-    
-    test('total likes test', () => {
-        const result = listHelper.totalLikes(lists)
-        expect(result).toBe(12)
+    test('total likes of the whole blog list', () => {
+      const result = listHelper.totalLikes(blogLists.blogs)
+      expect(result).toBe(36)
+    })
+
+    test('total likes of one blog', () => {
+      const result = listHelper.totalLikes(blogLists.listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('total likes of empty list', () => {
+      const result = listHelper.totalLikes(blogLists.emptyList)
+      expect(result).toBe(0)
     })
 })

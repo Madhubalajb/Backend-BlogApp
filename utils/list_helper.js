d40adecd56eae831def29c6c4d6b0f45412851e7
Madhubalajb <jbmadhumng@gmail.com>
2019-10-30 15:41:35 +0530

    unit test ex 4.7*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -36,16 +36,19 @@ const mostBlogs = (blogs) => {
 
 const mostLikes = (blogs) => {
     const fav = favoriteBlog(blogs)
-    const fav_author = fav.author
-    let fav_likes = 0;
-    blogs.forEach((item, index) => {
-        if(item.author === fav_author)
-            fav_likes += item.likes
-    })
-    return {
-        author: fav_author,
-        likes: fav_likes
+    if(fav !== undefined) {
+        const fav_author = fav.author
+        let fav_likes = 0;
+        blogs.forEach((item, index) => {
+            if(item.author === fav_author)
+                fav_likes += item.likes
+        })
+        return {
+            author: fav_author,
+            likes: fav_likes
+        }
     }
+    return []
 }
 
 module.exports = {

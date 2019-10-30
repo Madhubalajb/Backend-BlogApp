b6fe00322102f57727e40876dfcb55f448cec52d
Madhubalajb <jbmadhumng@gmail.com>
2019-10-30 13:40:49 +0530

    unit test ex 4.6*

---
+++ tests/mostBlogs.test.js
@@ -0,0 +1,22 @@
+const listHelper = require('../utils/list_helper')
+const blogList = require('../utils/blogLists')
+
+describe('most blogs', () => {
+    test('should return an empty array', () => {
+        expect(listHelper.mostBlogs(blogList.emptyList)).toEqual({})
+    })
+
+    test('should return the author of blog for the list of one blog', () => {
+        expect(listHelper.mostBlogs(blogList.listWithOneBlog)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            blogs: 1
+        })
+    })
+
+    test('should return the author of blog for the list of blogs', () => {
+        expect(listHelper.mostBlogs(blogList.blogs)).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -13,8 +13,31 @@ const favoriteBlog = (blogs) => {
     return fav[0]
 }
 
+const mostBlogs = (blogs) => {
+    const authors = []
+    blogs.forEach((item, index) => {
+    let temp = authors.find(item => item.author === blogs[index].author)
+    if(temp !== undefined) {
+        let temp_index = authors.findIndex(item => item.author === blogs[index].author)
+        authors[temp_index].blogs += 1
+    }
+    else {
+        authors.push(
+            {
+              author: blogs[index].author,
+              blogs: 1
+            }
+          )
+        }
+    })
+    const max = Math.max(...authors.map(item => item.blogs))
+    const result = authors.filter(item => item.blogs == max)
+    return result[0]
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }

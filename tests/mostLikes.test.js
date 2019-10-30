bf383d8db2c18c7a8f1936cf305dd4f3774e08dc
Madhubalajb <jbmadhumng@gmail.com>
2019-10-30 15:30:21 +0530

    unit test ex 4.7*

--- tests/mostLikes.test.js
+++ tests/mostLikes.test.js
@@ -3,6 +3,20 @@ const blogList = require('../utils/blogLists')
 
 describe('most likes', () => {
     test('should return empry array', () => {
-        
+        expect(listHelper.mostLikes(blogList.emptyList)).toEqual(blogList.emptyList[0])
+    })
+
+    test('should return author of blog for the list of one blog', () => {
+        expect(listHelper.mostLikes(blogList.listWithOneBlog)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+
+    test('should return author of blog with most likes from the list', () => {
+        expect(listHelper.mostLikes(blogList.blogs)).toEqual({
+            author: 'Robert C. Martin',
+            likes: 12
+        })
     })
 })

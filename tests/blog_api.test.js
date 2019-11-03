cccdeeae6c1e16dc6fa6001ff85bae8f5014eff4
Madhubalajb <jbmadhumng@gmail.com>
2019-11-03 10:30:36 +0530

    add test helper fns

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
 
 test('blogs are returned as json', async () => {
     await api
@@ -15,6 +15,15 @@ test('identifier property of the blogs', async () => {
     const response = await api.get('/api/blogs')
 
     expect(response.body[0].id).toBeDefined()
+})
+
+test('new blog addition', async () => {
+    const newBlog = {
+        title: 'unit testing in progress',
+        author: 'Madhubala Jayakumaran',
+        url: 'www.dummy.in',
+        likes: 1
+    }
 })
 
 afterAll(() => {
---
+++ tests/test_helper.js
@@ -0,0 +1,34 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'first blog',
+        author: 'Madhubala Jayakumaran',
+        url: 'www.first_blog.in',
+        likes: 2
+    },
+    {
+        title: 'second blog',
+        author: 'someone',
+        url: 'www.second_blog.in',
+        likes: 3
+    }
+]
+
+const nonExistingId = async () => {
+    const blog = new Blog({title: 'removed soon', author: 'noone', url: 'www.removed.in'})
+    await blog.save()
+    await blog.remove()
+    
+    return blog._id.toString()
+}
+
+const notesInDb = async () => {
+    const blogs = await Blog.find({})
+    
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs, nonExistingId, notesInDb
+}

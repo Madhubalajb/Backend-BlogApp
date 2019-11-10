514c754e5daec56f3166d55a5d5c22491b308d69
Madhubalajb <jbmadhumng@gmail.com>
2019-11-10 16:32:55 +0530

    4.15: bloglist expansion, step4

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,12 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 
+usersRouter.get('/', async (request, response, next) => {
+    const users = await User.find({})
+
+    response.json(users.map(user => user.toJSON()))
+})
+
 usersRouter.post('/', async (request, response, next) => {
     try {
         const body = request.body

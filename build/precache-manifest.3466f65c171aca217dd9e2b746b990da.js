007632dc0b09f9586595730c3c1cd494bfc986e1
Madhubalajb <jbmadhumng@gmail.com>
2019-12-26 22:21:55 +0530

    ui-build

---
+++ build/precache-manifest.3466f65c171aca217dd9e2b746b990da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707a25391904a91738da41a1df9f7198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a807db14832a8bf7d41",
+    "url": "/static/js/2.dbdf2a35.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.dbdf2a35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f38b1f67c6c314b331e2",
+    "url": "/static/js/main.3d358743.chunk.js"
+  },
+  {
+    "revision": "caf204c8969c70fbb3f5",
+    "url": "/static/js/runtime-main.6e761866.js"
+  }
+]);
